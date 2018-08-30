977eedbe3ca4b250cb79f15a72fb50778b6065b0
mcy <201102430@cs-cnu.org>
2018-08-30 18:47:06 +0900

    feat(setActor): user interface 추가

--- delta/src/index.js
+++ delta/src/index.js
@@ -3,6 +3,7 @@ import { extend } from "./snippet";
 export default class Delta {
     constructor(state) {
         this._state = state;
+        this._actors = {};
         return {
             setState: this.setState.bind(this),
         }
@@ -12,6 +13,10 @@ export default class Delta {
         this._state = extend(this._state, state);
         this._trigger(...Object.keys(state));
         return this._state;
+    }
+
+    setActor(key, actor) {
+        this._actors[key] = actor;
     }
 
     _trigger(...keys) {
