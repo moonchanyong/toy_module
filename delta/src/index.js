f5a35d163eda9c6752cd0d56ac88192b772c980d
mcy <201102430@cs-cnu.org>
2018-09-02 20:08:32 +0900

    feat(translate): 전이 실행 함수추가

--- delta/src/index.js
+++ delta/src/index.js
@@ -5,21 +5,36 @@ export default class Delta {
         this._state = option.state || {};
         this._transitions = option.transitions || {};
         return {
-            setState: this.setState.bind(this),
+            _setState: this._setState.bind(this),
             setTransition: this.setTransition.bind(this),
-        }
+            translate: this.translate.bind(this),
+        };
     }
 
-    setState(state) {
-        this._state = extend(this._state, state);
-        this._trigger(...Object.keys(state));
+    // getter
+    get state() {
+        return this._state;
     }
 
+    // setter
+
+    // Interface
     setTransition(transitions) {
         this._transitions = extend(this._transitions, transitions);
     }
 
+    translate(transition, payload) {
+        const nextState = this._transitions[transition](this.state, payload);
+        this._setState(nextState);
+    }
+
+    // private
     _trigger(...keys) {
 
+    }
+
+    _setState(state) {
+        this._state = extend(this._state, state);
+        this._trigger(...Object.keys(state));
     }
 }
